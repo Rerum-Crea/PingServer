ef2eaa34dfa4082d9686ab886a7afcc6f23cb1bf
Isaac <79072303+Necrownyx@users.noreply.github.com>
2022-04-02 16:21:25 +1000

    Did some things

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Necrownyx",
+    name="PingServer-Necrownyx",
     version="0.0.1",
     author="Isaac",
     author_email="no_reply@example.com",
     description="Makes creating a ping server easier",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Necrownyx/PingServer",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/Necrownyx/PingServer/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
+    package_dir={"": "PingServer"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
