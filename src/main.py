d36cb8c5d92ce758951be1b3df82175187846eda
Necronwyx <necrownyx@outlook.com>
2022-04-06 11:58:03 +1000

    Fix spelling mistake in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # Imports only the PingServer package to create, start, and run the servers on a thread. You might need to goto the
 # shell and import PingServer with: pip import pingserver==0.0.20 The version is because I actively work on the
-# package and sometimes their are bugs with the code and this is the current most stable version at the time of
+# package and sometimes there are bugs with the code and this is the current most stable version at the time of
 # writing this. Documentation at https://pypi.org/project/PingServer/
 
 
